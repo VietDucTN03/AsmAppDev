8dd6329cb4da790cfd3d301c7470a527fc9820db
VietDucTN03 <vietductn281103@gmail.com>
2024-04-23 09:56:59 +0700

    Add project files.

---
+++ AsmAppDev/Models/Job.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AsmAppDev.Models
+{
+	public class Job 
+	{
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public string requiredQualification { get; set; }
+		public DateTime Deadline { get; set; }
+
+		[ValidateNever]
+		public int CategoryId { get; set; }
+		[ForeignKey("CategoryId")]
+
+		[ValidateNever]
+		public Category Category { get; set; }
+	}
+}
